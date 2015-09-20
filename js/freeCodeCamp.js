b49e7b92924d0f88965eb215a1de9621723f5bae
ZoeAde <zoe.a.adelman@gmail.com>
2015-09-20 16:26:34 -0600

    Adding new solution

--- js/freeCodeCamp.js
+++ js/freeCodeCamp.js
@@ -1,9 +1,9 @@
-//Return the string in reverse order
+//REVERSE: Return the string in reverse order
 function reverseString(str) {
   return str.split('').reverse().join('');
 }
 
-//Return the factorial of the provided integer
+//FACTORIAL: Return the factorial of the provided integer
 function factorialize(num) {
   var factorial = 1;
   for(i = 2; i <= num; i++) {
@@ -14,6 +14,24 @@ function factorialize(num) {
   return factorial;
 }
 
-//
+//PALINDROME: Return true if the given string is a palindrome. Otherwise, return false.
+function palindrome(str) {
+  var newString = str.toLowerCase().replace(/[.,-\/#!$%\^&\*;:{}=\-_`~()/\s/]/g,"");
+  var reverse = newString.split("").reverse().join("");
+  return newString == reverse;
+};
 
+//LONGEST: Return the length of the longest word in a given sentence.
+function findLongestWord(str) {
+  var array = str.split(" ");
+  var longest = 0;
+  for (var i = 0; i < array.length; i++) {
+    if (array[i].length > longest) {
+      longest = array[i].length;
+    }
+  }
+  return longest;
+}
+
+//CAPITALIZE: Return the provided string with the first letter of each word capitalized. Make sure the rest of the word is in lower case.
 
